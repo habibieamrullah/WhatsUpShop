58f67a2d18c1474da00b31d74f3fd47c4f94c3d4
Habibie <habibieamrullah@gmail.com>
2021-06-14 10:43:22 +0700

    don't use whitespace for tableprefix

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost";
-$tableprefix = "whatastore_"; //You can change this table prefix
+$tableprefix = "whatastore_"; //You can change this table prefix, don't use white spaces, use underscores instead for example this_is_my_table_prefix_
 $databasename = "mydatabase";
 $dbuser = "root";
 $dbpassword = "";
